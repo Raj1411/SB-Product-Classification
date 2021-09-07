d5701ed6369eef9961de7acc8e01e48c57642481
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2021-09-07 14:17:44 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -112,12 +112,12 @@ else:
                 
     else:
         img1=Image.open(file)
-        st.image(img1,width=300)
+        st.image(img1.rotate(-90),width=300)
 #         st.image(img1,width=300) 
         height_percent1 = (fixed_height / float(img1.size[1]))
         width_size = int((float(img1.size[0]) * float(height_percent1)))
         new_img1 = img1.resize((width_size, fixed_height), Image.ANTIALIAS)
-#         new_img1=new_img1.rotate(270)
+        new_img1=new_img1.rotate(-90)
             # new_img1.save(file)
         reader=file_reader()
         with st.spinner("🤖  AI is Working....!"):
